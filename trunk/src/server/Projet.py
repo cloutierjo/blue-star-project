419ddf2368bf92c4e3386d33b60566b7c68c9ea1
francoislahey <francoislahey@c223113e-1d3c-11de-9c9a-2d325a3e6494>
2009-04-10 04:57:15 +0000

    Changement d'idée, à moins d'objection la clé primaire sera le nom du projet.

--- trunk/src/server/Projet.py
+++ trunk/src/server/Projet.py
@@ -17,7 +17,6 @@ class Projet(object):
         Constructor
         '''
         self.NOMPJ="nompj"
-        self.NUMPJ="numpj"  # TO BE APPROVED
         self.NOM="nom"
         self.VERBE="verbe"
         self.ADJECTIF="adjectif"
@@ -25,16 +24,14 @@ class Projet(object):
       #  self.ANALISEIMPLICITE="analyseImplicite"
         self.ANALISEEXPLICITE="analyseExplicite"
         self.nom = None
-        self.num = None   # TO BE APPROVED
         self.mandat = None
         self.analyseExplicite = []
         
     def serialize(self):
-        return {self.NOMPJ:self.nom,self.NUMPJ:self.num,self.MANDAT:self.mandat,self.ANALISEEXPLICITE:self.analyseExplicite}
+        return {self.NOMPJ:self.nom,self.MANDAT:self.mandat,self.ANALISEEXPLICITE:self.analyseExplicite}
     
     def deserialize(self, serializedProject):
         self.nom=serializedProject[self.NOMPJ]
-        self.num=serializedProject[self.NUMPJ]
         self.mandat=serializedProject[self.MANDAT]
         self.analyseExplicite=serializedProject[self.ANALISEEXPLICITE]
         
@@ -57,11 +54,10 @@ class Projet(object):
                 row[self.NOM] = unicode(row[self.NOM])
                 row[self.VERBE] = unicode(row[self.VERBE])
                 row[self.ADJECTIF] = unicode(row[self.ADJECTIF])
-            
+                
 if __name__ == '__main__':
     pj=Projet()
     pj.nom="project Name"
-    pj.num=1
     pj.mandat="ceci est un tres tres grand projet comprenant beaucoup d'idée... encore incomplete"
     pj.addItemAnaliseExplicite("projet", "faire", "tres long")
     pj.addItemAnaliseExplicite("idée","comprant","beaucoup")
@@ -70,6 +66,3 @@ if __name__ == '__main__':
     print pj.serialize()
     pj.deserialize(pj.serialize())
     print pj.serialize()
-    
-    
-    
