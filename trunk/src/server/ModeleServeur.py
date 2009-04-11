5e6f1a1884b42b6542f2d0be131ca6e49d7b8311
francoislahey <francoislahey@c223113e-1d3c-11de-9c9a-2d325a3e6494>
2009-04-11 21:58:30 +0000

    Finalement ajout de la variable num dans projet qui sert d'ID dans la BD

--- trunk/src/server/ModeleServeur.py
+++ trunk/src/server/ModeleServeur.py
@@ -12,7 +12,7 @@ class ModeleServeur:
     # Constructeur
     def __init__(self):
         
-        self.db = 'test1.bd'                    # cheminFichierDB
+        self.db = ':memory:'                    # cheminFichierDB
         self.con = sqlite3.connect(self.db)     # Connecteur
     
     # Initialisation de premier demarrage (Creation BD/Tables)
@@ -20,9 +20,12 @@ class ModeleServeur:
     
         cur = self.con.cursor()     # Curseur
         
-        cur.execute('''create table Projets(nom VARCHAR2(30), mandat text)''')
-        cur.execute('''create table Analyses(projet text, nom text, verbe text, adjectif text)''')
-    
+        cur.execute('''CREATE TABLE Projets(ID NUMBER(6) PRIMARY KEY, Nom VARCHAR2(50), Mandat LONG)''')
+        cur.execute('''CREATE TABLE Analyses(ID NUMBER(6) REFERENCES Projets, nom VARCHAR2(30), verbe VARCHAR2(30), adjectif VARCHAR2(30))''')
+        # En attendant de trouver comment créer une séquence, cette table sert de sauvegarde pour une séquence pour les ID
+        cur.execute('''CREATE TABLE Seq(Val NUMBER(6))''')
+        cur.execute('insert into Seq values(?)', (1,))
+        
         cur.close()
         
     # Lecture d'un projet dans la BD et affectation dans les variables.
@@ -37,24 +40,65 @@ class ModeleServeur:
         
         pass
     
-    # Sauvegarde les donnees d'un projet dans la BD
-    def sauvegardeProjet(self, projet):
+    # Sauvegarde les donnees d'un projet dans la BD, renvoie True si réussi sinon renvoie false
+    def saveProject(self, projet):
         
+        saved = False
         projet.unicodize()          # Unicodize le projet
-        cur = self.con.cursor()     # Curseur
         
-        # Update table Projets
-        entryTableProjets = (projet.nom, projet.mandat) # Nouvelle entrée
-        cur.execute('insert into Projets values(?, ?)', entryTableProjets)
+        # Si c'est un nouveau projet... on le cré
+        if projet.num == 0:
+            
+            cur = self.con.cursor()     # Curseur
+            
+            projet.num = self.getNewID()
+            
+            # Ajout du projet dans la table Projets
+            entryTableProjets = (projet.num, projet.nom, projet.mandat) # Nouvelle entrée
+            cur.execute('insert into Projets values(?, ?, ?)', entryTableProjets)
+            
+            # Update table Analyses 
+            for row in projet.getAnaliseExpliciteTuple():
+                # Ajout du nom du projet pour la sauvegarde dans la BD
+                toSave = (projet.num, row[0], row[1], row[2])
+                cur.execute('insert into Analyses values(?, ?, ?, ?)', toSave)
+                
+            cur.close()
+            saved = True
+            
+        # Sinon... on update les tables nécéssaire. 
+        else:
+            
+            cur = self.con.cursor()     # Curseur
+            
+            # Update table Projets
+            #entryTableProjets = (projet.num, projet.nom, projet.mandat) # Nouvelle entrée
+            #cur.execute('insert into Projets values(?, ?, ?)', entryTableProjets)
+            
+            # Update table Analyses 
+            cur.execute('DELETE FROM Analyses WHERE ID = (?)', (projet.num,))
+            for row in projet.getAnaliseExpliciteTuple():
+                # Ajout du nom du projet pour la sauvegarde dans la BD
+                toSave = (projet.num, row[0], row[1], row[2])
+                cur.execute('insert into Analyses values(?, ?, ?, ?)', toSave)
+                
+            cur.close()
+            saved = True
+            
+        return saved
         
-        # Update table Analyses 
-        for row in projet.getAnaliseExpliciteTuple():
-            # Ajout du nom du projet pour la sauvegarde dans la BD
-            toSave = (projet.nom, row[0], row[1], row[2])
-            
-            cur.execute('insert into Analyses values(?, ?, ?, ?)', toSave)
-            
+    # Sert de séquence de nombre pour l'ID des projet en attendant de trouver comment faire une séquence
+    def getNewID(self):
+        
+        cur = self.con.cursor()    # Curseur
+        
+        cur.execute('''Select Val from Seq''')
+        for row in cur:
+            val = row[0]
+        cur.execute('UPDATE Seq SET Val = (?)', (val+1,))
+    
         cur.close()
+        return val
     
     # Methode de DEBUGAGE
     def test(self):
@@ -65,19 +109,20 @@ class ModeleServeur:
         print "Table Projets :"
         cur.execute('''Select * from Projets''')
         for row in cur:
-            ligne1 = row[0]
-            ligne2 = row[1]
-            print ligne1+" : "+ligne2
+            id = row[0]
+            nom = row[1]
+            mandat = row[2]
+            print str(id)+" : "+nom+" : "+mandat
             
         print ""
         print "Table Analyses : "
         cur.execute('''Select * from Analyses''')
         for row in cur:
-            champ1 = row[0]
+            id = row[0]
             champ2 = row[1]
             champ3 = row[2]
             champ4 = row[3]
-            ligne = champ1+", "+champ2+", "+champ3+", "+champ4
+            ligne = str(id)+", "+champ2+", "+champ3+", "+champ4
             print ligne
             
         cur.close()
@@ -86,7 +131,7 @@ class ModeleServeur:
 if __name__ == "__main__":
     
     ms = ModeleServeur()        # Creation du ModeleServeur
-    # ms.initDB()               # TO BE CALLED FOR FIRST USE ON A SERVER (CREATE TABLES)
+    ms.initDB()                 # TO BE CALLED FOR FIRST USE ON A SERVER (CREATE TABLES)
     
     # Creation d'un Projet    
     p=Projet()                  
@@ -97,7 +142,9 @@ if __name__ == "__main__":
     p.addItemAnaliseExplicite("une cerise","maché","rouge")
     p.addItemAnaliseExplicite("roger","sucoté","inconsciemment")
        
-    ms.sauvegardeProjet(p)      # Test de sauvegarde d'un projet
-    ms.test()                   # Check DB integrity
-    ms.sauvegardeProjet(p)      # Test de sauvegarde d'un projet
+    print ms.saveProject(p)    # Test de sauvegarde d'un projet
+    ms.test()                       # Check DB integrity
+    p.num = 0
+    print ms.saveProject(p)    # Test de sauvegarde d'un projet
+    ms.test()                       # Check DB integrity
      
