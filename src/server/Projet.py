e6f259f9cf762ed6de3487a995e4cba590969427
mathieulavoie020 <mathieulavoie020@c223113e-1d3c-11de-9c9a-2d325a3e6494>
2009-04-20 13:31:44 +0000

    Modification du projet intégration de cas d'usage et scénario d'utilistation

--- src/server/Projet.py
+++ src/server/Projet.py
@@ -4,7 +4,7 @@ Created on 8 avr. 2009
 
 @author: Jonatan Cloutier
 '''
-
+from Analyse import *
 class Projet(object):
     '''
     contient tout les informations d'un projet ainsi que des méthode 
@@ -62,31 +62,7 @@ class Projet(object):
         self.analyseExplicite.unicodize()
         self.analyseImplicite.unicodize()
         
-class Analyse:
-    
-    NOM="nom"
-    VERBE="verbe"
-    ADJECTIF="adjectif"
-    
-    def __init__(self,parent):
-            
-        self.analyse = []
-        self.parent = parent
-    
-    def getForDB(self):
-        anExpTup=[]
-        for item in self.analyse:
-            anExpTup.append((self.parent.num,item[self.NOM],item[self.VERBE],item[self.ADJECTIF]))
-        return anExpTup
-    
-    def addItem(self, nom, verbe, adjectif):
-        self.analyse.append({self.NOM:nom,self.VERBE:verbe,self.ADJECTIF:adjectif})
-        
-    def unicodize(self):
-        for row in self.analyse:
-            row[self.NOM] = unicode(row[self.NOM])
-            row[self.VERBE] = unicode(row[self.VERBE])
-            row[self.ADJECTIF] = unicode(row[self.ADJECTIF])
+
 
 if __name__ == '__main__':
     pj=Projet()
---
+++ src/server/ScenarioUtilisation.py
@@ -0,0 +1,12 @@
+class ScenarioUtilisation:
+    def __init__(self):
+        self.etapes = []
+        
+    def ajouterEtape(self,uneEtape,apres = -1):
+        
+        if apres == -1:
+            self.etapes.append()
+        else:
+            self.etapes
+            for i in range(len(self.etapes),apres,-1):
+                pass
